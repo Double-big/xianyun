f347930b00645c92874cd0196e6d5f7c3c29ad9c
dada <1113052033@qq.com>
2020-07-18 16:10:03 +0800

    带参数传送到机票列表页

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,15 +1,19 @@
 <template>
-  <div>
-      机票列表
-  </div>
+  <div>机票列表</div>
 </template>
 
 <script>
 export default {
-
-}
+  created() {
+    this.$axios({
+      url: "/airs",
+      params: this.$route.query
+    }).then(res => {
+      console.log(res.data);
+    });
+  }
+};
 </script>
 
 <style>
-
 </style>
